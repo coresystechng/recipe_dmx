d7602294cbfc4418799d0102ce81399143ba4a68
Collins Okoroafor <collins.okoroafor@outlook.com>
2023-09-26 04:44:00 +0100

    How to delete data

--- view_recipe.php
+++ view_recipe.php
@@ -2,8 +2,24 @@
 //COnnect to database
 include('templates/connect.php');
 
-//Declare empty variable
-$recipe_id = '';
+//Declare empty variables
+$recipe_id = $delete_id = '';
+
+if(isset($_POST['delete'])) {
+    $delete_id = $_POST['delete_id'];
+
+    $delete_query = "DELETE FROM `recipe_tb` WHERE recipe_id = '$delete_id'";
+
+    $send_delete_query = mysqli_query($connect, $delete_query);
+
+    if($send_delete_query){
+        header('Location: index.php');
+    } else {
+        echo 'could not delete' , mysqli_connect_error($connect);
+    }
+
+    mysqli_close($connect);
+}
 
 //Check if variable is set 
 if(isset($_GET['recipe_id'])){
@@ -34,4 +50,17 @@ if(isset($_GET['recipe_id'])){
 
 <h1><?php echo $recipe['recipe_name'] ?></h1>
 <p>created by: <?php echo $recipe['created_at']?></p>
+<p class="green-text text-darken-4">How to Prepare:</p>
+<p><?php echo $recipe['recipe_description'] ?></p>
+
+    <form action="view_recipe.php" method="POST">
+        <div class="row">
+            <div class="col s1">
+                <input type="text" name="delete_id" hidden value="<?php echo $recipe['recipe_id']; ?>">
+            </div>
+            <br>
+        </div>
+        <input type="submit" value="delete" name="delete" class="btn btn-flat green darken-4 white-text">
+    </form>
+
 <?php  include('templates/footer.php')?>
